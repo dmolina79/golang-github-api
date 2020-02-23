af430c35ae165764360e94ff4cc559488fc00663
dmolina79 <doug.molinam@gmail.com>
2020-02-22 18:06:42 -0600

    add mock from testify

--- src/api/controllers/repositories/repositories_controller_test.go
+++ src/api/controllers/repositories/repositories_controller_test.go
@@ -7,38 +7,43 @@ import (
 	"github.com/dmolina79/golang-github-api/src/api/utils/errors"
 	"github.com/dmolina79/golang-github-api/src/api/utils/test_utils"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
 )
 
-var (
-	funcCreateRepo func (request repositories.CreateRepoRequest) (*repositories.CreateRepoResponse, errors.ApiError)
-	funcCreateRepos func (request []repositories.CreateRepoRequest) (repositories.CreateReposResponse, errors.ApiError)
-)
-
-type repoServiceMock struct {}
-
-func (s *repoServiceMock) CreateRepo(request repositories.CreateRepoRequest) (*repositories.CreateRepoResponse, errors.ApiError) {
-	return funcCreateRepo(request)
+type repoServiceMock struct {
+	mock.Mock
 }
 
-func (s *repoServiceMock) CreateRepos(request []repositories.CreateRepoRequest) (repositories.CreateReposResponse, errors.ApiError) {
-	return funcCreateRepos(request)
+// stubs for mock
+func (r repoServiceMock) CreateRepo(request repositories.CreateRepoRequest) (*repositories.CreateRepoResponse, errors.ApiError) {
+	args := r.Called(request)
+	if args.Error(1) != nil {
+		return nil, args.Error(1).(errors.ApiError)
+	}
+
+	return args.Get(0).(*repositories.CreateRepoResponse), nil
+
+}
+
+func (r repoServiceMock) CreateRepos(request []repositories.CreateRepoRequest) (repositories.CreateReposResponse, errors.ApiError) {
+	return repositories.CreateReposResponse{}, nil
 }
 
 func TestCreateRepo_Success(t *testing.T) {
-	services.RepositoryService = &repoServiceMock{}
-
-	// setup mock
-	funcCreateRepo = func(request repositories.CreateRepoRequest) (*repositories.CreateRepoResponse, errors.ApiError) {
-		return &repositories.CreateRepoResponse{
-			Id:    123,
+	mockService := new(repoServiceMock)
+	mockService.On("CreateRepo", mock.Anything).Return(
+		&repositories.CreateRepoResponse{
+			Id:    321,
 			Owner: "vbuterin",
 			Name:  "github-repo-test-mock",
-		}, nil
-	}
+		},
+		nil)
+
+	services.RepositoryService = mockService
 
 	request, _ := http.NewRequest(http.MethodPost, "/repo", strings.NewReader(`{ "name": "github-repo"}`))
 	response := httptest.NewRecorder()
@@ -50,7 +55,29 @@ func TestCreateRepo_Success(t *testing.T) {
 	var result repositories.CreateRepoResponse
 	err := json.Unmarshal(response.Body.Bytes(), &result)
 	assert.Nil(t, err)
-	assert.EqualValues(t, 123, result.Id)
+	assert.EqualValues(t, 321, result.Id)
 	assert.EqualValues(t, "vbuterin", result.Owner)
 	assert.EqualValues(t, "github-repo-test-mock", result.Name)
+}
+
+func TestCreateRepo_HandleError(t *testing.T) {
+	mockService := new(repoServiceMock)
+	mockService.On("CreateRepo", mock.Anything).Return(
+		nil, errors.NewApiError(http.StatusBadRequest, "error on request"))
+
+	services.RepositoryService = mockService
+
+	request, _ := http.NewRequest(http.MethodPost, "/repo", strings.NewReader(`{ "name": "github-repo"}`))
+	response := httptest.NewRecorder()
+	c := test_utils.GetMockContext(request, response)
+
+	CreateRepo(c)
+
+	assert.EqualValues(t, http.StatusBadRequest, response.Code)
+
+	apiErr, err := errors.NewApiErrFromBody(response.Body.Bytes())
+	assert.Nil(t, err)
+	assert.NotNil(t, apiErr)
+	assert.EqualValues(t, http.StatusBadRequest, apiErr.Status())
+	assert.EqualValues(t, "error on request", apiErr.Message())
 }
